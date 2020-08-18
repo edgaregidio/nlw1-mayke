4c96dec2bdcae6e728501f05d853297bb86a8f16
edgaregidio <edgarsiqueirasi@gmail.com>
2020-08-18 15:36:27 -0300

    configurando nunjucks

--- src/server.js
+++ src/server.js
@@ -4,16 +4,23 @@ const server = express()
 // Configurar a pasta public
 server.use(express.static('public'))
 
+// Utilizando template engine
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+})
+
 // Configurar caminhos da aplicação
 // Paga inicial
 server.get("/", (req, res) => {
-  return res.sendFile(__dirname + '/views/index.html')
+  return res.render('index.html')
 })
 server.get("/create-point", (req, res) => {
-  return res.sendFile(__dirname + '/views/create-point.html')
+  return res.render('create-point.html')
 })
 server.get("/search-results", (req, res) => {
-  return res.sendFile(__dirname + '/views/search-results.html')
+  return res.render('search-results.html')
 })
 // Ligar o servidor
 server.listen(3000)
