e8046d85f1fd662d153da3f16b17dc78e8fdf7bf
edgaregidio <edgarsiqueirasi@gmail.com>
2020-08-18 15:25:15 -0300

    configuração de rotas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const server = express()
 
+// Configurar a pasta public
+server.use(express.static('public'))
 
+// Configurar caminhos da aplicação
+// Paga inicial
+server.get("/", (req, res) => {
+  return res.sendFile(__dirname + '/views/index.html')
+})
+server.get("/create-point", (req, res) => {
+  return res.sendFile(__dirname + '/views/create-point.html')
+})
+server.get("/search-results", (req, res) => {
+  return res.sendFile(__dirname + '/views/search-results.html')
+})
 // Ligar o servidor
-
 server.listen(3000)
