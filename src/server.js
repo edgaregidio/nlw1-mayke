b00a47d90c0fc4f0032253c49648a162ee808abf
edgaregidio <edgarsiqueirasi@gmail.com>
2020-08-20 17:40:21 -0300

    captura de dados finalizada

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const db = require('./database/db');
 
 // Configurar a pasta public
 server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }))
 
 // Utilizando template engine
 const nunjucks = require('nunjucks')
@@ -19,28 +20,56 @@ server.get("/", (req, res) => {
   return res.render('index.html', { title: "Seu marketplace de coleta de resíduos" })
 })
 
-
-
 server.get("/create-point", (req, res) => {
-
-  console.log(req.query)
   return res.render('create-point.html')
 })
 
 server.post('/savepoint', (req, res) => {
-  return res.send('ok')
+  console.log(req.body)
+
+  // inserir dados no banco de dados
+  const query = `
+      INSERT INTO places (
+        name,
+        image,
+        address,
+        address2,
+        state,
+        city,
+        items
+      ) VALUES (
+        ?, ?, ?, ?, ?, ?, ?
+      );  
+  `
+  const values = [
+    req.body.name,
+    req.body.image,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertDate(err) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log("Cadastrado com Sucesso")
+    console.log(this)
+  }
+
+  db.run(query, values, afterInsertDate)
+  return res.render('create-point.html', {saved: true })
 })
 
 server.get("/search", (req, res) => {
-
   // Consultar dados
   db.all(`SELECT * FROM places`, function (err, rows) {
     if (err) {
       return console.log(err)
     }
-
     const total = rows.length
-
     // mostrar a pagina html com os dados do banco de dados
     return res.render('search-results.html', { places: rows, total: total })
   })
